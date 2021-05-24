7a835a02acfae5c3f7d48506d187112aebb9e612
Dominik 'Rengyr' Kosík <dominik6kosik@gmail.com>
2021-05-24 17:48:04 +0200

    Added verbose levels for printing added/removed files and for all files.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ extern crate crc32fast;
 
 use clap::{App, Arg};
 use crc32fast::Hasher;
+use same_file::is_same_file;
 use std::collections::{HashMap, HashSet, VecDeque};
+use std::ffi::OsStr;
 use std::fs::{metadata, File, Metadata};
 use std::io::{BufRead, ErrorKind, Read, Write};
 use std::path::{Path, PathBuf};
 use std::{fs, io, process};
-use same_file::is_same_file;
-use std::ffi::OsStr;
 
 fn main() {
     let matches = App::new("Hashify")
@@ -35,7 +35,13 @@ fn main() {
                 .long("buffer")
                 .takes_value(true)
                 .help("Size of read buffer in KB"),
-        )
+        ).arg(
+        Arg::with_name("verbose")
+            .short("v")
+            .multiple(true)
+            .help("Sets the level of verbosity: (repeat for increased verbosity)\n\tLevel 0: No verbose info\n\tLevel 1: Verbose info about new/removed files\n\tLevel 2: Verbose info about every file
+                    "),
+    )
         .arg(
             Arg::with_name("input")
                 .takes_value(true)
@@ -48,6 +54,12 @@ fn main() {
     let recursive: bool = matches.is_present("recursive");
 
     let output: &str = matches.value_of("output").unwrap_or("");
+
+    let verbose:u8 = match matches.occurrences_of("verbose") {
+        0 => 0,
+        1 => 1,
+        2 | _ => 2,
+    };
 
     let buffer_size: usize = matches
         .value_of("buffer")
@@ -132,8 +144,11 @@ fn main() {
                         };
                         let file_name = file.path();
                         if file.file_type().unwrap().is_file() {
-                            let file_os_name= file.path().as_os_str().to_string_lossy().into_owned();
-                            if file_name.file_name().unwrap() == output_file_name && is_same_file(&file_name, output_path).unwrap_or(false){
+                            let file_os_name =
+                                file.path().as_os_str().to_string_lossy().into_owned();
+                            if file_name.file_name().unwrap() == output_file_name
+                                && is_same_file(&file_name, output_path).unwrap_or(false)
+                            {
                                 continue;
                             }
                             let hash = match crc32_hash(file_name, buffer_size) {
@@ -145,20 +160,20 @@ fn main() {
                                     continue;
                                 }
                             };
-                            if hashes_old.contains_key(
-                                &file_os_name,
-                            ) {
-                                let old_hash = hashes_old
-                                    .get(&file_os_name)
-                                    .unwrap();
+                            if hashes_old.contains_key(&file_os_name) {
+                                let old_hash = hashes_old.get(&file_os_name).unwrap();
                                 if *old_hash != hash {
                                     eprintln!("File hash mismatch:\n\tFile: {}\n\tOld Hash: {:08x}\n\tNew Hash: {:08x}", file_os_name, old_hash, hash);
                                 }
+                                else if verbose >= 2{
+                                    println!("Known file found:\n\tFile: {}\n\tHash: {:08x}", file_os_name, hash);
+                                }
                             }
-                            hashes.insert(
-                                file_os_name,
-                                hash,
-                            );
+                            else if verbose >= 1{
+                                println!("New file found:\n\tFile: {}\n\tHash: {:08x}", file_os_name, hash);
+                            }
+
+                            hashes.insert(file_os_name, hash);
                         } else if recursive && file.file_type().unwrap().is_dir() {
                             if !seen.contains(&file_name) {
                                 seen.insert(file_name.clone());
@@ -173,6 +188,13 @@ fn main() {
                     println!("{:08x}\t{}", hash, file);
                 }
             } else {
+                if verbose >=1 {
+                    let removed_iter = hashes_old.iter().filter(|x|!hashes.contains_key(x.0));
+                    for (file, hash) in removed_iter {
+                        println!("Removed file found:\n\tFile: {}\n\tHash: {:08x}", file, hash);
+                    }
+                }
+
                 let mut f = match File::create(output) {
                     Ok(file) => file,
                     Err(e) => {
@@ -180,9 +202,9 @@ fn main() {
                         process::exit(1);
                     }
                 };
-                let mut iter:Vec<(&String, &u32)> = hashes.iter().collect();
-                iter.sort_by(|(file_a,_),(file_b, _)|file_a.cmp(file_b));
-                for (file, hash) in iter{
+                let mut iter: Vec<(&String, &u32)> = hashes.iter().collect();
+                iter.sort_by(|(file_a, _), (file_b, _)| file_a.cmp(file_b));
+                for (file, hash) in iter {
                     match f.write_all(format!("{:08x}\t{}\n", hash, file).as_bytes()) {
                         Ok(_) => {}
                         Err(e) => {
